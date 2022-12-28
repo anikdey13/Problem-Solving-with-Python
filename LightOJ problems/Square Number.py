0c0714f2b981ee6253338e104ef76c3058c97e00
Anik Dey <deyanik659@gmail.com>
2022-12-28 23:28:57 +0600

    n

--- LightOJ problems/Square Number.py
+++ LightOJ problems/Square Number.py
@@ -1,12 +1,21 @@
 import math
 
-T = int(input())
+# T = int(input())
 
+# for i in range(T):
+#     N = int(input())
+#     l = math.sqrt(N)
+#     if l * l == N:
+#         print('YES')
+#     else:
+#         print('NO')
+
+T = int(input())
 
 for i in range(T):
     N = int(input())
-    l = math.sqrt(N)
-    if l * l == N:
+    root = math.sqrt(N)
+    if int(root + 0.5) ** 2 == N:
         print('YES')
     else:
         print('NO')
